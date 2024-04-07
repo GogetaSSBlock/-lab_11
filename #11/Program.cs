e3ed8763703c65b34d13f9dc7975e628452fed3d
GogetaSSBlock <165895953+GogetaSSBlock@users.noreply.github.com>
2024-04-07 14:19:12 +0300

    Add project files.

---
+++ #11/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public interface IScalable
+{
+    void Scale(double s);
+}
+
+public class Circle : IScalable
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public void Scale(double s)
+    {
+        Radius *= s;
+    }
+
+    public override string ToString()
+    {
+        return $"Коло з радiусом {Radius}";
+    }
+}
+
+public class Rectangle : IScalable
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public void Scale(double s)
+    {
+        Width *= s;
+        Height *= s;
+    }
+
+    public override string ToString()
+    {
+        return $"Прямокутник з шириною {Width} та висотою {Height}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 3);
+
+        Console.WriteLine("Початковий:");
+        Console.WriteLine(circle);
+        Console.WriteLine(rectangle);
+
+        circle.Scale(10);
+        rectangle.Scale(20);
+
+        Console.WriteLine("\nПiсля масштабування:");
+        Console.WriteLine(circle);
+        Console.WriteLine(rectangle);
+    }
+}
